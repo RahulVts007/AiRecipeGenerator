cad28ff3a9d5bd72a74b22d247d57b55dbf9a526
RahulVts007 <rahulvts89@gmail.com>
2026-03-15 11:20:49 +0530

    Configured Databse

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,23 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
 
+dotenv.config();
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/' , (req , res) => {
+    res.json({message: 'API Working'});
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT , () => {
+    console.log(`Server Running on Port ${PORT}`)
+})
 
