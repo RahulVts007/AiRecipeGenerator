822389c789d040ef167b25ee7863462010002dff
RahulVts007 <rahulvts89@gmail.com>
2026-03-26 14:41:10 +0530

    UserPreferences Model Done

---
+++ backend/models/UserPrefrences.js
@@ -0,0 +1,49 @@
+import db from '../config/db.js'
+
+class UserPreferences {
+
+    // Create or Uodate User Prefrences
+
+    static async upsert(userId , preferences){
+        const {
+            dietary_resstrictions = [],
+            allergies = [],
+            preferred_cuisines = [],
+            default_servings = 4,
+            measurement_unit = 'metric'
+        } = preferences;
+
+        const result = await db.query(
+            `INSERT INTO user_preferences
+            (user_id , dietary_restrictions , allergies , preferred_cuisines , default_servings , measurement_unit)
+            VALUES ($! , $2 , $3 , $4 , $5 , $6)
+            ON CONFLICT
+            DO UPDATE SET
+             dietary_restrictions = $2,
+             allergies = $3,
+             preferred_cuisines = $4,
+             default_servings = $5,
+             measurement_unit = $6
+            RETURNING *`,
+            [userId , dietary_resstrictions , allergies , preferred_cuisines , default_servings , measurement_unit]
+        );
+
+        return result.rows[0];
+    }
+
+    static async findByUserId(userId) {
+        const result = await db.query(
+            `SELECT * FROM user_preferences WHERE user_id = $1`,
+            [userId]
+        );
+
+        return result.rows[0] || null;
+    }
+
+    static async delete(userId){
+        await db.query(`DELETE FROM user_preferences WHERE user_id = $1` , [userId]);
+    }
+
+}
+
+export default UserPreferences;
